4bf2a46323d70325231409b79c492bec3022e6ad
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-03-27 15:52:46 -0400

    updated footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Container, Image, Row, Col } from "react-bootstrap";
+import snftr_logo from './images/SNFTR_ico_wh85.png';
 
 function Footer() {
-return (
-  <div>
-    <h2>This is where footer will be</h2>
-  </div>
-)
+
+  return (
+    <Container className="footer">
+      <Row className="justify-content-center fixed-bottom mt-5" style={{color: "#9003a2"}}>
+        <p><small>© 2022 Esoteric Productions</small></p>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer;
