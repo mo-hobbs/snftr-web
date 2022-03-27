3215b77366f89638df885110b8dc0e2fb72cf1e3
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-03-27 16:14:59 -0400

    updated footer (again)

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,15 +1,19 @@
 import React from "react";
-<<<<<<< Updated upstream
-=======
 import { Container, Row } from "react-bootstrap";
->>>>>>> Stashed changes
 
 function Footer() {
-return (
-  <div>
-    <h2>This is where footer will be</h2>
-  </div>
-)
+  return (
+    <Container className="footer">
+      <Row
+        className="justify-content-center fixed-bottom mt-5"
+        style={{ color: "#9003a2" }}
+      >
+        <p>
+          <small>© 2022 Esoteric Productions</small>
+        </p>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer;
