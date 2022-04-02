5b22175c37d5e6f8318a610effe5f72637deabc1
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-04-02 13:17:12 -0400

    installed firebase

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,24 +5,29 @@ import google_play_button from "./images/get-it-on-google-play-button.png";
 import app_store_button from "./images/download-on-app-store-button.png";
 
 function Home() {
-
   const buttonSize = {
-    width: "15em"
+    width: "15em",
   };
   return (
-    <Row>
-      <Col>
-        <h2>the SNFTR App</h2>
-        <p>the best place to find, save, and share gifs</p>
-        <Row >
-        <Col ><Image src={google_play_button} style={buttonSize} alt="download the app on Google Play"></Image></Col>
-        <Col><Image src={app_store_button} alt="download the app on Apple Store" style={buttonSize}></Image></Col>
-        </Row>
-      </Col>
-      <Col>
-      <Image></Image>
-
-      </Col>
+    <Row className="mt-5">
+      <h2>the SNFTR App</h2>
+      <p>the best place to find, save, and share gifs</p>
+      <Row className="mt-3">
+        <Col>
+          <Image
+            src={google_play_button}
+            style={buttonSize}
+            alt="download the app on Google Play"
+          ></Image>
+        </Col>
+        <Col>
+          <Image
+            src={app_store_button}
+            alt="download the app on Apple Store"
+            style={buttonSize}
+          ></Image>
+        </Col>
+      </Row>
     </Row>
   );
 }
